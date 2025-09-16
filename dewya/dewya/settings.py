09598e3edc522dba36479af6cc0921b2481d701a
Devananda K <devanandak@Devanandas-MacBook-Pro.local>
2025-09-16 18:07:35 +0530

    prepare project for render deployment

--- dewya/dewya/settings.py
+++ dewya/dewya/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-!5v5r9j90dy%qkihaqwl$1#+*dorf)bju_=!h+^h3@tjns3iv@"
 
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -57,6 +57,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
