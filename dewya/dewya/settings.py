eb2e33f02e86ba5c6e25880caae4924a87cfc196
Devananda K <devanandak@Devanandas-MacBook-Pro.local>
2025-09-17 01:23:46 +0530

    changed

--- dewya/dewya/settings.py
+++ dewya/dewya/settings.py
@@ -130,9 +130,11 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_URL = '/static/'
 # STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
