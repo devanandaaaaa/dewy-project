3a1d313e8e1be3fb1422d7431e01d3be39f12a2a
Devananda K <devanandak@Devanandas-MacBook-Pro.local>
2025-09-19 18:19:01 +0530

    changes

--- dewya/store/views.py
+++ dewya/store/views.py
@@ -31,7 +31,7 @@ def product_list(request):
     })
 
 def product_detail(request, slug):
-    product = get_object_or_404(Product, slug=slug, available=True)
+    product = get_object_or_404(Product, slug=slug)
     return render(request, 'store/product_detail.html', {'product': product})
 
 def offers_page(request):
